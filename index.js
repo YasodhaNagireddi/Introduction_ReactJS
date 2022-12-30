6935f60cf518c46697e6bc9b26eb489b20ace654
YasodhaNagireddi <yasodhanagireddi07@gmail.com>
2022-12-30 17:44:34 +0000

    Introduction to ReactJS

---
+++ index.js
@@ -0,0 +1,8 @@
+const name = "Yasodha";
+const element = (
+  <div>
+    <h1 className="greeting">Hello,{name}!</h1>
+    <p>Good to see you here.</p>
+  </div>
+);
+ReactDOM.render(element, document.getElementById("root"));
